99cfb60dd1b49922cf5a5df8f1b2f5896aefe32a
ramakrishna67 <ramakrishnapaila64@gmail.com>
2024-04-27 01:35:14 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from mdb import create_user, inchc_bal, dechc_bal, login, get_usrdata, hf_data
+
+app = Flask(__name__)
+
+@app.route('/account_create' , methods=['POST'])
+def create_user():
+    username=str(request.form['username'])
+    email=str(request.form['email'])
+    password=str(request.form['password'])
+    location=str(request.form['location'])
+    return create_user(username, email, password, location)
+
+
+@app.route('/account_login' , methods=['POST'])
+def login():
+    username=str(request.form['username'])
+    password=str(request.form['password'])
+    return login(username, password)
+
+@app.route('/get_data' , methods=['POST'])
+def get_usrdata():
+    username=str(request.form['username'])
+    return get_usrdata(username)
+
+@app.route('/helpform_data' , methods=['POST'])
+def hf_data():
+    username=str(request.form['username']) 
+    title=str(request.form['title'])
+    description=str(request.form['description'])
+    location=str(request.form['location'])
+    hc_pay=str(request.form['hc_pay'])
+    return hf_data(username,title,description,location,hc_pay)
